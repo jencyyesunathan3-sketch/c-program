1788cfddb893441ffaa59f75ae60150734e961a5
jencyyesunathan3-sketch <jencyyesunathan3@gmail.com>
2026-07-30 21:48:52 +0530

    Add files via upload

---
+++ Area of Triangle.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    float area,radius;
+
+    printf("Enter the radius:");
+    scanf("%f",&radius);
+
+    area = 2*3.14*radius;
+    printf("Area of Circle:%2f\n",area);
+
+    return 0;
+}
+
