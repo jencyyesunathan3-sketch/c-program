d297dbb0ad6c98c8df5f18891be8f00e0613f6ce
jencyyesunathan3-sketch <jencyyesunathan3@gmail.com>
2026-07-30 20:05:15 +0530

    Add files via upload

---
+++ Primitive Data Type C.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int A = 10;
+    printf("A:%d\n",A);
+
+    char ch = 'a';
+    printf("ch:%c\n",ch);
+
+    float B = 12.45;
+    printf("B:%f\n",B);
+
+    double C = 12.3456789;
+    printf("C:%lf\n",C);
+
+
+    return 0;
+}
