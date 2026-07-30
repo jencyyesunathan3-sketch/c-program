7203b3850dd3dc3e19af205479fe03abe67bd3c4
jencyyesunathan3-sketch <jencyyesunathan3@gmail.com>
2026-07-30 19:18:20 +0530

    Add files via upload

---
+++ Assignment Operator C.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int a=20,b=10;
+
+    printf("a+=b:%d\n",a+=b);
+    printf("a-=b:%d\n",a-=b);
+    printf("a*=b:%d\n",a*=b);
+    printf("a/=b:%d\n",a/=b);
+    printf("a%=b:%d\n",a%=b);
+    printf("a&=b:%d\n",a&=b);
+    printf("a|=b:%d\n",a|=b);
+    printf("a^=b:%d\n",a^=b);
+    printf("a>>=b:%d\n",a>>=b);
+    printf("a<<=b:%d\n",a<<=b);
+    printf("a>>=1:%d\n",a>>=1);
+    printf("a<<=1:%d\n",a<<=1);
+
+    return 0;
+}
