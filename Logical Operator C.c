c03def57d884dfeb8953ef27f2db3679cc30bf35
jencyyesunathan3-sketch <jencyyesunathan3@gmail.com>
2026-07-30 18:54:48 +0530

    Add files via upload

---
+++ Logical Operator C.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a=20,b=10;
+
+    printf("a&&b:%d\n",a&&b);
+    printf("a||b:%d\n",a||b);
+    printf("!a:%d\n",!a);
+    printf("!b:%d\n",!b);
+
+    return 0;
+}
