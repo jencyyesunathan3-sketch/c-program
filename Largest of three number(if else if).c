12d9783e0ece138bacc9c022ee9722c43a1a30bf
jencyyesunathan3-sketch <jencyyesunathan3@gmail.com>
2026-07-31 15:35:05 +0530

    Add files via upload

---
+++ Largest of three number(if else if).c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter the three numbers:");
+    scanf("%d%d%d",&a,&b,&c);
+
+    if(a>b && a>c)
+    {
+        printf("a is a greatest number");
+    }
+    if(b>a && b>c)
+    {
+        printf("b is a greatest number");
+    }
+    else
+    {
+        printf("c is a greatest number");
+    }
+    return 0;
+}
