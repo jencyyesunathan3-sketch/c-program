d3efdc1e9a715c3b0f4564148c0925310b92ce1b
jencyyesunathan3-sketch <jencyyesunathan3@gmail.com>
2026-07-31 16:09:13 +0530

    Add files via upload

---
+++ Leap year or not(if else).c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int year;
+    printf("Enter the year:");
+    scanf("%d",&year);
+
+    if((year%400=2=0)||(year%4==0&&year%100!=0))
+    {
+        printf("It is a leap year");
+    }
+    else
+    {
+        printf("Not a leap year");
+    }
+    return 0;
+}
