5af81bd2035fd76c119a2c318ab0dbb6e8c5bddf
jencyyesunathan3-sketch <jencyyesunathan3@gmail.com>
2026-07-31 19:57:50 +0530

    Add files via upload

---
+++ Sum of naturals numbers(goto).c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,i=1,sum=0;
+    printf("Enter n:");
+    scanf("%d",&n);
+
+    Start:
+        if(i<=n)
+        {
+            sum=sum+i;
+            i++;
+
+        goto Start;
+        }
+
+        printf("Sum:%d",sum);
+        return 0;
+}
