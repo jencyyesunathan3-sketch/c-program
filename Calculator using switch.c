28ac7e13b929b69b76b25ff1fa589c4d2ec61811
jencyyesunathan3-sketch <jencyyesunathan3@gmail.com>
2026-07-31 16:29:33 +0530

    Add files via upload

---
+++ Calculator using switch.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,choice;
+    printf("Enter two numbers:");
+    scanf("%d%d",&a,&b);
+
+    printf("1.Add\n,2.Sub\n,3.Multi\n,4.Div\n:");
+    printf("Enter the choice(1-4):");
+    scanf("%d",&choice);
+
+    switch(choice)
+    {
+    case 1:
+        printf("Add:%d\n",a+b);
+        break;
+
+    case 2:
+        printf("Sub:%d\n",a-b);
+        break;
+
+    case 3:
+        printf("Multi:%d\n",a*b);
+        break;
+
+    case 4:
+        printf("Div:%d",a/b);
+        break;
+    }
+    return 0;
+}
+
