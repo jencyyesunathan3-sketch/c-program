5d5e13cde82d5184877e2a04fd661057dbe30479
jencyyesunathan3-sketch <jencyyesunathan3@gmail.com>
2026-07-31 12:39:22 +0530

    Add files via upload

---
+++ if statement example.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int age;
+    printf("Enter age:");
+    scanf("%d",&age);
+
+    if(age==20)
+    {
+        printf("Eligible to vote");
+    }
+
+
+    return 0;
+}
