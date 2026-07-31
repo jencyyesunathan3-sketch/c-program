e711537461a397c68d9f97abb63d0ff54ff9cce8
jencyyesunathan3-sketch <jencyyesunathan3@gmail.com>
2026-07-31 12:21:38 +0530

    Add files via upload

---
+++ Positive or Negative C.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter n:");
+    scanf("%d",&n);
+
+    if(n>=0)
+    {
+        printf("Positive");
+    }
+    else
+     {
+         printf("Negative");
+     }
+
+    return 0;
+}
