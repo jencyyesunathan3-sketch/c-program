55c18ebc6fb3c70df7957b1822f4e9f1791b943d
jencyyesunathan3-sketch <jencyyesunathan3@gmail.com>
2026-08-01 10:45:31 +0530

    Add files via upload

---
+++ Power of x using for loop.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,x;
+    long int p=1;
+
+    printf("Enter the x and n:");
+    scanf("%d%d",&x,&n);
+
+    for(i=1;i<=n;i++)
+    {
+        p=p*x;
+        printf("%d",p);
+    }
+
+    return 0;
+}
