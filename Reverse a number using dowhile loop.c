707e4146d3d4e3ebdc64ce84a74d0b3fc557fa87
jencyyesunathan3-sketch <jencyyesunathan3@gmail.com>
2026-08-01 11:28:57 +0530

    Add files via upload

---
+++ Reverse a number using dowhile loop.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,rev=0;
+    printf("Enter n:");
+    scanf("%d",&n);
+
+    do
+    {
+        rev=rev*10+n%10;
+        n=n/10;
+    }while(n>0);
+
+    printf("Reverse:%d",rev);
+    return 0;
+}
