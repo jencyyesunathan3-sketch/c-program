458cbebc950abb871450820b894eecfbba61bc46
jencyyesunathan3-sketch <jencyyesunathan3@gmail.com>
2026-08-01 12:33:14 +0530

    Add files via upload

---
+++ Armstrong number using while loop.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n,temp,rem,sum=0;
+    printf("Enter n:");
+    scanf("%d",&n);
+
+    temp=n;
+
+    while(temp>0)
+    {
+        rem=temp%10;
+        sum=sum+rem*rem*rem;
+        temp=temp/10;
+    }
+    if(sum==n)
+    {
+        printf("Armstrong");
+    }
+    else
+    {
+        printf("Not Armstrong");
+    }
+    return 0;
+}
