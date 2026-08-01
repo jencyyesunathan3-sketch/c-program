f43fe1fab8456c32185dd1cc0f255f7cef64fad3
jencyyesunathan3-sketch <jencyyesunathan3@gmail.com>
2026-08-01 10:18:12 +0530

    Add files via upload

---
+++ Sum of odd numbers using for loop.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,sum=0;
+    printf("Enter the n:");
+    scanf("%d",&n);
+
+    for(i=1;i<=n;i=i+2)
+        sum=sum+i;
+        printf("Sum of Natural Numbers:%d",sum);
+    return 0;
+}
