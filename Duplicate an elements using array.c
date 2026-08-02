caea61949a839ec463ecb0980383f5d637ef3f03
jencyyesunathan3-sketch <jencyyesunathan3@gmail.com>
2026-08-02 11:49:13 +0530

    Add files via upload

---
+++ Duplicate an elements using array.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int arr[100],n,i,j,count=0;
+    printf("Enter the number of elements:");
+    scanf("%d",&n);
+
+    printf("Enter the elements:");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(arr[i]==arr[j])
+            {
+                count++;
+                break;
+            }
+        }
+    }
+    printf("The duplicate elements:%d",count);
+    return 0;
+}
