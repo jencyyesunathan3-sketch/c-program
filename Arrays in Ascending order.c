50634d5019a1b35b1c576839aedb7d7ae4112a3b
jencyyesunathan3-sketch <jencyyesunathan3@gmail.com>
2026-08-02 11:36:46 +0530

    Add files via upload

---
+++ Arrays in Ascending order.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int arr[100],i,j,n,temp;
+    printf("Enter the numbers:");
+    scanf("%d",&n);
+
+    printf("Enter the elements");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",arr[i]);
+    }
+    for(i=0;i<n-1;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(arr[i]>arr[j])
+            {
+                temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+    }
+    printf("Array in Ascending Order:");
+    for(i=0;i<n;i++)
+    {
+        printf("%d",arr[i]);
+    }
+    return 0;
+}
