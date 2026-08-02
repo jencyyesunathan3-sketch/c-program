564db2e6b6cad4add245cd8065445d6b0e177070
jencyyesunathan3-sketch <jencyyesunathan3@gmail.com>
2026-08-02 23:49:56 +0530

    Add files via upload

---
+++ Transpose of matrix using Array.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+    int a[10][10],transpose[10][10];
+    int i,j,rows,cols;
+
+    printf("Enter number of rows:");
+    scanf("%d",&rows);
+    printf("Enter number of columns:");
+    scanf("%d",&cols);
+
+    printf("Enter the elements of matrix:");
+    for(i=0;i<rows;i++)
+    {
+        for(j=0;j<cols;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<rows;i++)
+    {
+        for(j=0;j<cols;j++)
+        {
+            transpose[j][i]=a[i][j];
+        }
+    }
+    printf("Transpose of Matrix:");
+    for(i=0;i<cols;i++)
+    {
+        for(j=0;j<rows;j++)
+        {
+            printf("%d",transpose[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
